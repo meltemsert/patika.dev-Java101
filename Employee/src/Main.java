1290aaedb10d68c6d6f710d33586db590077163d
Meltem Sert <meltemsert29@gmail.com>
2022-10-24 14:37:40 +0300

    Practicum homework done

--- Employee/src/Main.java
+++ Employee/src/Main.java
@@ -2,11 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Employee employee = new Employee();
-        employee.setEmployeeName("MELTEM SERT");
-        employee.setEmployeeSalary(2000.0);
-        employee.setEmployeeWorkHours(45);
-        employee.setEmployeeHireYear(1985);
+        Employee employee = new Employee("MELTEM SERT",2000.0,45,1985);
         employee.toString();
 
 
